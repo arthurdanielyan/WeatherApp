1a8110c0c36327b8e9e506ed48b490295018b423
arturdanielyan1 <arturdanielyan19@gmail.com>
2022-10-19 23:26:23 +0400

    replaced provides function with dagger-hilt abstract injection

--- app/src/main/java/com/bignerdranch/android/weather/feature_5_days_forecast/data/DataModule.kt
+++ app/src/main/java/com/bignerdranch/android/weather/feature_5_days_forecast/data/DataModule.kt
@@ -1,20 +1,20 @@
 package com.bignerdranch.android.weather.feature_5_days_forecast.data
 
-import com.bignerdranch.android.weather.core.data.api.WeatherApi
 import com.bignerdranch.android.weather.feature_5_days_forecast.data.repository.FiveDaysForecastRepositoryImpl
 import com.bignerdranch.android.weather.feature_5_days_forecast.domain.repository.FiveDaysForecastRepository
+import dagger.Binds
 import dagger.Module
-import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object DataModule {
+abstract class DataModule {
 
-    @Provides
+    @Binds
     @Singleton
-    fun provideFiveDaysForecastRepository(weatherApi: WeatherApi): FiveDaysForecastRepository =
-        FiveDaysForecastRepositoryImpl(weatherApi)
+    abstract fun bindFiveDaysForecastRepository(
+        fiveDaysForecastRepositoryImpl: FiveDaysForecastRepositoryImpl
+    ): FiveDaysForecastRepository
 }
