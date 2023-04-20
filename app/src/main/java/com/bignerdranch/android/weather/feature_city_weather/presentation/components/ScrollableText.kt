c5744ecaaca61b5cac06be4fed84506bbd8100a8
arturdanielyan1 <arturdanielyan19@gmail.com>
2023-04-21 01:05:26 +0400

    scrollable text finnaly works

--- app/src/main/java/com/bignerdranch/android/weather/feature_city_weather/presentation/components/ScrollableText.kt
+++ app/src/main/java/com/bignerdranch/android/weather/feature_city_weather/presentation/components/ScrollableText.kt
@@ -10,8 +10,13 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.pointer.pointerInteropFilter
+import androidx.compose.ui.layout.boundsInParent
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
 import com.bignerdranch.android.weather.core.constants.log
 import com.bignerdranch.android.weather.core.extensions.putInRange
 
@@ -19,22 +24,26 @@ import com.bignerdranch.android.weather.core.extensions.putInRange
 @Composable
 fun ScrollableText(
     modifier: Modifier = Modifier,
-
     textModifier: Modifier = Modifier,
     text: String,
     style: TextStyle = LocalTextStyle.current
 ) {
 
+    
+
     var scrolledLetters by remember { mutableStateOf(0) }
     var initialX by remember { mutableStateOf(0f) }
-    val scrolledLettersRanged = scrolledLetters.putInRange(0, text.length-2)
+    val scrolledLettersRanged = scrolledLetters.putInRange(0, text.length - 2)
     val actualText = text.substring(scrolledLettersRanged)
     var lastTouchX by remember { mutableStateOf(0f) }
+    var textWidth by remember { mutableStateOf(0.dp) }
+    var rowWidth by remember { mutableStateOf(0.dp) }
+    val density = LocalDensity.current
 
     Row(
         modifier = modifier
             .pointerInteropFilter { motionEvent ->
-                when(motionEvent.action) {
+                when (motionEvent.action) {
                     ACTION_DOWN -> {
                         initialX = motionEvent.x
                     }
@@ -42,20 +51,29 @@ fun ScrollableText(
                         val currentX = motionEvent.x
                         val swipedLeft = currentX - lastTouchX < 0
                         lastTouchX = currentX
-                        log(initialX.toInt() % currentX.toInt())
+                        log(
+                            try {
+                                initialX.toInt() % currentX.toInt()
+                            } catch (e: ArithmeticException) {
+                                e.localizedMessage
+                            }
+                        )
                         log("scrolledLetterRanged: $scrolledLettersRanged, actualText: $actualText, text: $text")
-                        if((initialX.toInt() - currentX.toInt())%5 == 0)
-                        if (swipedLeft) {
-                            if(actualText == text.substring(text.length-actualText.length))
+                        if ((initialX - currentX).toInt() % 3 == 0)
+                            if (swipedLeft) {
+                                if(textWidth >= rowWidth - 5.dp)
                                 scrolledLetters++
-                            log("scrolled forward $scrolledLetters")
-                        } else {
-                            scrolledLetters--
-                            log("scrolled backward $scrolledLetters")
-                        }
+                                log("scrolled forward $scrolledLetters")
+                            } else {
+                                scrolledLetters--
+                                log("scrolled backward $scrolledLetters")
+                            }
                     }
                 }
                 true
+            }
+            .onGloballyPositioned {
+                rowWidth = density.run { it.boundsInParent().size.width.toDp() }
             },
         horizontalArrangement = Arrangement.Center
     ) {
@@ -65,6 +83,9 @@ fun ScrollableText(
             maxLines = 1,
             style = style,
             overflow = TextOverflow.Ellipsis,
+            onTextLayout = { layoutResult ->
+                textWidth = density.run { layoutResult.size.width.toDp() }
+            }
         )
     }
 }
